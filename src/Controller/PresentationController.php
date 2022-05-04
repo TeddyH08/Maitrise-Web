cb433d2064408672525c69ce80658c3c62bd1bdd
TeddyH08 <teddy.hemonet@gmail.com>
2022-05-04 15:47:39 +0200

    contact fin

---
+++ src/Controller/PresentationController.php
@@ -0,0 +1,17 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class PresentationController extends AbstractController {
+
+        /**
+         * @Route("/presentation", name="presentation")
+         * @return Response
+         */
+        public function index(): Response {
+            return $this->render('pages/presentation.html.twig');
+        }
+    }
