6f19124d68ef47fb2d303b924a010bed2c556da1
TeddyH08 <teddy.hemonet@gmail.com>
2022-05-09 15:40:17 +0200

    amelioration

---
+++ src/Controller/ProjetController.php
@@ -0,0 +1,17 @@
+<?php
+    namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class ProjetController extends AbstractController {
+
+        /**
+         * @Route("/projet", name="projet")
+         * @return Response
+         */
+        public function index(): Response {
+            return $this->render('pages/projet.html.twig');
+        }
+    }
